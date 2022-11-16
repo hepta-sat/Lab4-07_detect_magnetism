30dd16b5e4b8267f92030b7df0e1c968b8e44ab2
Charlie.com <csru22005@g.nihon-u.ac.jp>
2022-11-16 00:43:05 +0000

    hepta

--- main.cpp
+++ main.cpp
@@ -1,12 +1,20 @@
 #include "mbed.h"
+#include "HEPTA_EPS.h"
+#include "HEPTA_SENSOR.h"
+ 
+RawSerial pc(USBTX,USBRX,9600);
+HEPTA_EPS eps(p16,p26);
+HEPTA_SENSOR sensor(p17,
+                  p28,p27,0x19,0x69,0x13,
+                  p13, p14,p25,p24);
 
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+int main()
+{
+    float mx,my,mz;
+    pc.printf("Magnetometer Mode\r\n");
+    for(int i = 0; i<50; i++) {
+        sensor.sen_mag(&mx,&my,&mz);
+        pc.printf("mag : %f,%f,%f\r\n",mx,my,mz);
+        wait(1.0);
+    }
 }
